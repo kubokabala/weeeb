e6e0267b970936f62323bfd799426aa2f5e9862b
kubokabala <40245735+kubokabala@users.noreply.github.com>
2021-11-15 11:22:38 +0300

    пятая лаба без LocalStorage

---
+++ Lab5/list.js
@@ -0,0 +1,39 @@
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const result = document.querySelector("#result");
+const total = document.querySelector("#total");
+let i = 0;
+
+btn.addEventListener('click', (e) => {
+    if (input.value === "") return;
+    createDeleteElements(input.value);
+    input.value = "";
+})
+
+function createDeleteElements(value) {
+    i++;
+    console.log(value)
+
+    const btn = document.createElement("button");
+    const li = document.createElement("li");
+
+    li.className = "li";
+    li.textContent = value;
+
+    btn.className = "btn";
+    btn.textContent = "Delite";
+    li.appendChild(btn);
+
+    btn.addEventListener("click", (e) => {
+        result.removeChild(li);
+        i--;
+        total.textContent = i;
+    })
+
+    li.addEventListener("click", (e) => {
+        li.classList.toggle("li-dead");
+    })
+
+    total.textContent = i;
+    result.appendChild(li)
+}
